df180e1ef8bcd9dcec6b37587b5221bd177d60c0
Ricardo E. Galvez <galvez667@gmail.com>
2018-03-15 07:23:51 -0400

    :meat_on_bone:  enough mucking

--- themes/blank/static/js/main.js
+++ themes/blank/static/js/main.js
@@ -188,49 +188,6 @@ function windowResized() {
 /// Mostly the idea is to change the background color
 /// based on the time.
 
-// some colors... in an array;
-// might be easier in the future to work w/ HSL instead of Hex...
-const bgcolors = [
-  "#333",
-  "#4C4",
-  "#567",
-  "#789",
-  "#ABC",
-  "#AF0",
-  "#333",
-  "#1AC",
-  "#567",
-  "#7D9",
-  "#AFC",
-  "#A10",
-  "#3D3",
-  "#4A4",
-  "#537",
-  "#729",
-  "#A1C",
-  "#9F0",
-  "#333",
-  "#444",
-  "#5A7",
-  "#7E9",
-  "#1B2",
-  "#4D0",
-  "#7B9",
-  "#D09"
-];
-
-// where we want to put a 0 in front of our number,
-// since we're passing the value of time directly as a numeric corallary to our array, we dont want a zero. leaving here for ref. though.
-// function addZero(i) {
-//   if (i < 10) {
-//     i = "0" + i;
-//   }
-//   return i;
-// }
-// var h = addZero(d.getUTCHours() - 5);
-// var m = addZero(d.getUTCMinutes());
-
-
 // get the time
 function getHours() {
   const d = new Date();
@@ -239,46 +196,8 @@ function getHours() {
   let h = d.getUTCHours() - offset;
   let absoluteFix = (Math.abs(h));
   h = h - absoluteFix;
-//return h;
-//return offset;
 return absoluteFix;
 }
-
-// function fixTime() {
-//   let fix = getHours();
-//   console.log("this is the fix " + fix);
-//    if (fix <= 19) {
-//      fix = (fix % 19);
-//     //console.log("heyo " + (Math.abs(fix)));
-//
-//     //console.log("the abs fix " + absoluteFix);
-//    }
-//    //  else {
-//    //   //fix = fix % 19;
-//    //   //fix = 19 % fix;
-//    //   console.log("pendejo " + fix);
-//    // }
-// }
-
-
-// function setBackGround() {
-  // const container = document.querySelector('body');
-  //console.log(container);
-  // look at the array of colors assign the colors sequentially
-  // if its between 0 & 0:59 change the bg color to the first item in the color array [0], if its between 1 & 1:59 change the color the second item in the array [1], etc
-  // let theHour = getHours();
-  //console.log(theHour);
-  // set the background color to the corollary position in the bgColors array
-//   container.style.backgroundColor = bgcolors[theHour];
-// }
-
-//setBackGround();
-
-///
-/// Now now dot the graient works
-///
-// hsl colors would be better than a bunch of hex numbers...
-
 
 const bgPairs = [
   {
@@ -292,12 +211,6 @@ const bgPairs = [
 ];
 
 
-
-//
-// let ft = fixTime();
-// console.log(ft);
-
-
 // Now set them to the hour:
 const theHour = getHours();
 console.log(theHour);
@@ -306,15 +219,8 @@ const end = bgPairs[0].end[theHour];
 const alpha = 0.25;
 const container = document.querySelector('body');
 
-
-
-// console.log("the current hour: " + theHour);
-// console.log("the start hex: " + bgPairs[0].start[theHour]);
-// console.log("the end hex: " + bgPairs[0].end[theHour]);
-
 function setTheGradient() {
-  container.style.backgroundImage = 'linear-gradient(182deg, hsla(0, 5%, 5%, 0.5) 0%, hsla(182, 95%, 55%, 0.27)), linear-gradient(90deg,' + start + ',' + end + ')';
-  //container.style.backgroundImage = 'linear-gradient(182deg, hsla(0, 5%, 5%, 0.5) 0%, hsla(182, 95%, 55%, 0.27))';
+  container.style.backgroundImage = 'linear-gradient(182deg, hsla(0, 5%, 5%, 0.75) 0%, hsla(0, 0%, 100%, 0.25)), linear-gradient(90deg,' + start + ',' + end + ')';
 };
 
 setTheGradient();

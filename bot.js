f6826015018966986ee61a368f5383fe58d67f56
ricardo galvez <ricardogalvez@nypl.org>
2018-04-09 17:02:53 -0400

    :meat_on_bone: added in an express server. dont forget to NPM install

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
-console.log('the bot works');
+// our app.
+const express = require('express')
+const app = express();
 const config = require('./botConfig');
 const Twit = require('twit');
 const T = new Twit(config);
@@ -7,9 +9,10 @@ const T = new Twit(config);
 // Get the hastag to search with from the client
 //
 
+
 let params = {
-  q: '%23trumpsucks since:2018-04-4',
-  count: 1
+  q: '%23woodworking since:2016-01-1',
+  count: 5
 }
 
 function gotData(err, data, response) {
@@ -17,11 +20,13 @@ function gotData(err, data, response) {
   for (let i =0; i < tweets.length; i++) {
     //console.log(tweets[i].text);
   }
-  let theResponse = response;
-
-      console.log(theResponse);
-    
 }
 
 //T.get('search/tweets', params, gotData);
 let twitterReturn = T.get('search/tweets', params, gotData);
+
+//console.log('the bot works');
+
+app.get('/', (req, res) => res.send(twitterReturn))
+
+app.listen(3000, () => console.log('Twitter (bot.js) is doing the listening on port 3000! \n'))

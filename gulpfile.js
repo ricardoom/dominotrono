bb04517a8d66533104962d19f7a0c8fe840fe479
Ricardo E. Galvez <galvez667@gmail.com>
2019-02-23 17:53:01 -0500

    :meat_on_bone: using Hugo's build processes instead of the gulps! so much time sunk into the gulps...

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 'use strict';
 const gulp = require('gulp');
 
-const sass = require('gulp-sass');
+// const sass = require('gulp-sass');
 
-const sourcemaps = require('gulp-sourcemaps');
+// const sourcemaps = require('gulp-sourcemaps');
 
-const autoprefixer = require('gulp-autoprefixer');
+// const autoprefixer = require('gulp-autoprefixer');
 
 const concat = require('gulp-concat');
 
@@ -17,10 +17,9 @@ const pump = require('pump');
 
 const cssnano = require('gulp-cssnano');
 
-
 //
-const input = 'themes/blank/src/sass/**/*.scss';
-const output = 'themes/blank/static/css';
+// const input = 'themes/blank/src/sass/**/*.scss';
+// const output = 'themes/blank/static/css';
 
 const jsInput = 'themes/blank/src/js/**/*.js';
 const jsOutput = 'themes/blank/static/js';
@@ -28,14 +27,14 @@ const jsOutput = 'themes/blank/static/js';
 const imgInput = 'themes/blank/images/**/*.*';
 const imgOutput = 'themes/blank/static/images';
 
-const sassOptions = {
-  errLogToConsole: true,
-  outputStyle: 'compressed',
-};
+// const sassOptions = {
+//   errLogToConsole: true,
+//   outputStyle: 'compressed',
+// };
 
-const autoprefixerOptions = {
-  browsers: ['last 2 versions', '> 5%', 'Firefox ESR'],
-};
+// const autoprefixerOptions = {
+//   browsers: ['last 2 versions', '> 5%', 'Firefox ESR'],
+// };
 
 const babelOptions = {
   presets: ['@babel/env'],
@@ -55,37 +54,39 @@ gulp.task('babel', () => {
 
 // Concact all the files:
 gulp.task('js', () => {
-  return (gulp.src(jsInput)
-    .pipe(concat('main.min.js'))
-    // .pipe(uglify())
-    .pipe(gulp.dest(jsOutput))
+  return (
+    gulp
+      .src(jsInput)
+      .pipe(concat('main.min.js'))
+      // .pipe(uglify())
+      .pipe(gulp.dest(jsOutput))
   );
 });
 
 // Compress using uglify
-gulp.task('compress', function (cb) {
+gulp.task('compress', function(cb) {
   pump([gulp.src(jsInput), uglify(), gulp.dest(jsOutput)], cb);
 });
 
-gulp.task('sass', function () {
-  return gulp
-    .src(input)
-    .pipe(sass())
-    .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(sourcemaps.init())
-    .pipe(sass(sassOptions).on('error', sass.logError))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(output));
-});
+// gulp.task('sass', function () {
+//   return gulp
+//     .src(input)
+//     .pipe(sass())
+//     .pipe(autoprefixer(autoprefixerOptions))
+//     .pipe(sourcemaps.init())
+//     .pipe(sass(sassOptions).on('error', sass.logError))
+//     .pipe(sourcemaps.write('./'))
+//     .pipe(gulp.dest(output));
+// });
 
-gulp.task('copy', function () {
+gulp.task('copy', function() {
   gulp.src(imgInput).pipe(gulp.dest(imgOutput));
 });
 
-gulp.task('watch', function () {
-  gulp.watch('themes/blank/src/sass/**/*.scss', ['sass']);
+gulp.task('watch', function() {
+  // gulp.watch('themes/blank/src/sass/**/*.scss', ['sass']);
   gulp.watch('themes/blank/src/js/**/*.js', ['js']);
   gulp.watch('themes/blank/images/**/*.*', ['copy']);
 });
 
-gulp.task('default', ['copy', 'sass', 'js', 'watch']);
+gulp.task('default', ['copy', 'js', 'watch']);

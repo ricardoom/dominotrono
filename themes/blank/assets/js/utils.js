55e678970f82ff4d294a2110bd9ba89c5a64aeba
Ricardo E. Galvez <galvez667@gmail.com>
2021-02-15 22:40:22 -0500

    :meat_on_bone: guh... still working on gsap

--- themes/blank/assets/js/utils.js
+++ themes/blank/assets/js/utils.js
@@ -16,3 +16,9 @@ function getRandomPosNeg(num) {
   }
   return num % 2 ? num * -1 : num;
 }
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
--- themes/blank/static/js/experiments/atmos.js
+++ themes/blank/static/js/experiments/atmos.js
@@ -5,78 +5,27 @@
 // Atmosphere in this case
 // Get the parent wrapper node:
 
-const parentWrapper = document.querySelector('.wrapper');
+const parentWrapper = document.querySelector('article');
 // Get the DOM node:
 const svgContainer = document.querySelector('#atmosphere');
 
 // Get the viewport dimensions and set up two constants:
 const x = document.body.clientWidth; // use the client window width
 const y = document.body.clientHeight; // use the client window height
-const r = getRandomInt(25);
+const radius = getRandomIntInclusive(50, 200);
 
-//
-// set up the animation function
-// takes an options object as an argument
-const animator = (options) => {
-  let start = performance.now();
-  requestAnimationFrame(function animate(time) {
-    // timeFraction from 0 to 1
-    let timeFraction = (time - start) / options.duration;
-    if (timeFraction > 1) timeFraction = 1;
-    // current animation state:
-    // prop is the property we're animating
-    let props = options.timing(timeFraction);
-    options.draw(props);
-    if (timeFraction < 1) {
-      requestAnimationFrame(animate);
-    }
-  });
-};
-
-// we need to add or update the svg el on the page
-
-// we need to get the ID
-
-// we need alter the elements gracefully
-
-// paint them to the screen
 function makeCircles(numberOfCircles) {
-  // if there are already circles remove them:
-  //removeCircles(svgContainer);
-  // removeElementWithPizzaz(svgContainer);
   // make sure all of the elements are somewhere in the viewBox
   svgContainer.setAttribute('viewBox', `0 0 ${x} ${y}`);
-  // svgContainer.setAttribute('width', `${x - 100}`);
   for (let i = 0; i < numberOfCircles; i++) {
     const newCircle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
     newCircle.setAttribute('cx', getRandomInt(x));
     newCircle.setAttribute('cy', getRandomInt(y));
-    newCircle.setAttribute('r', getRandomInt(r));
-    // newCircle.style.color = `hsla(${getRandomInt(360)},${getRandomInt(100)}%, ${getRandomInt(100)}%`;
+    newCircle.setAttribute('r', getRandomInt(radius));
     newCircle.style.color = `hsla(0, 0, 100%, ${getRandomInt(100)}%)`;
     newCircle.style.fill = 'currentColor';
     newCircle.style.filter = `blur(${getRandomInt(3)}px)`;
     svgContainer.appendChild(newCircle);
-
-    // this object
-    // animation options:
-    // const animateTheCircles = {
-    //   duration: getRandomInt(1500),
-    //   timing(timeFraction) {
-    //     return timeFraction;
-    //   },
-    //   draw(opacity) {
-    //     newCircle.style.opacity = opacity - 0.1;
-    //   },
-    // };
-
-    // run the circle animation on each circle:
-    // function runAnimate() {
-    //   animator(animateTheCircles);
-    // }
-
-    // run the animation function:
-    // runAnimate();
   }
 }
 // show and animate them
@@ -84,16 +33,13 @@ function makeCircles(numberOfCircles) {
 // kick it all off
 makeCircles(getRandomInt(25));
 
-// Do Some Gsap stuff shall we?
+// GSAP animation
 // Do a simple check to see if gsap is available
+const circles = document.querySelectorAll('circle');
+const circArr = Array.from(circles);
 if (gsap) {
-  const circles = document.querySelectorAll('circle');
-  console.log(circles);
-  // circles.forEach(
-  //   // gsap.to('circle', {
-  //   //   x: x - 100,
-  //   //   duration: 0.5,
-  //   //   repeat: -1,
-  //   // })
-  // );
+  // get all the circles in the svg element
+
+  const opts = { x: x, duration: 10, repeat: -1, delay: 0.3 };
+  gsap.to(circArr, { opts });
 }

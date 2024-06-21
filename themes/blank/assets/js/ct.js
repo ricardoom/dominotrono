5859b1aef0aa18dd2fec787ad1cdd86bedd8b0cb
ricardoom <ricardo.e.galvez@gmail.com>
2024-06-21 15:04:57 -0700

    feat(Content Update): :lipstick: added new svg and image action to the home page
    getting a new collage like thing going on the hp
    
    none!

--- themes/blank/assets/js/ct.js
+++ themes/blank/assets/js/ct.js
@@ -5,7 +5,7 @@
 import params from "@params";
 // import { getRandomIntInclusive } from "./utils";
 import {Cloudinary} from '@cloudinary/url-gen';
-
+import {Resize} from '@cloudinary/url-gen/actions/resize';
 // console.log(params.artworks);
 const data = params.artworks;
 
@@ -19,6 +19,7 @@ const randImg = data[rand].display_name;
 // console.log(randImg);
 
 // Create your instance
+// TODO: sort out this double call to Cloudinary
 const cld = new Cloudinary({
   cloud: {
     cloudName: 'toupeeproofbullets'
@@ -27,22 +28,21 @@ const cld = new Cloudinary({
     secure: true // force https, set to false to force http
   }
 });
-
-
   
 // Let's create a new image
 const myImage = cld.image(randImg);
 
 // Import the resize transformation and apply it to myImage
-import {Resize} from '@cloudinary/url-gen/actions/resize';
-
 // Resize the image to 100x100
-myImage.resize(Resize.scale().width(960));
+myImage.resize(Resize.scale().width(1200));
 
 // When we're done, we can apply all our changes and create a URL.
 const myURL = myImage.toURL();
 
 // https://res.cloudinary.com/demo/image/upload/c_scale,w_100,h_100/sample
 console.log(myURL);
-
-console.log('am I working, maybe not?')
+// update the mastBG
+const siteContent = document.getElementById('siteContent');
+const mastimg = siteContent.querySelector('.mast-img');
+mastimg.setAttribute("src", myURL);
+console.log(mastimg);

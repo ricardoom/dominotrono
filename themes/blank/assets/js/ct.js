01644515c521f6daf6792b689a85dda206cea420
ricardoom <ricardo.e.galvez@gmail.com>
2024-07-11 12:25:53 -0700

    refactor(Behavior Update): :zap: Removing console logging 🪵

--- themes/blank/assets/js/ct.js
+++ themes/blank/assets/js/ct.js
@@ -1,7 +1,7 @@
 // Cloudinary Image Import and Transformations
 
 import params from "@params";
-import {Cloudinary} from '@cloudinary/url-gen';
+import { Cloudinary } from '@cloudinary/url-gen';
 import { Resize } from '@cloudinary/url-gen/actions/resize';
 // console.log(params.artworks);
 const data = params.artworks;
@@ -34,10 +34,10 @@ const myURL = myImage.toURL();
 const baseURL = document.location;
 const [local, remote] = ['http://localhost:1313/', "https://ricardogalvez.design/"];
 // console.log(baseURL === local || remote);
-console.log(baseURL.href, local === baseURL.href)
+// console.log(baseURL.href, local === baseURL.href)
 
 if (baseURL.href === local || baseURL.href === remote) {
-  console.log('its local...', baseURL.href)
+  // console.log('its local...', baseURL.href)
   const siteContent = document.querySelector('.site-content');
   const mastimg = siteContent.querySelector('.mast-img');
   mastimg.setAttribute("src", myURL);
